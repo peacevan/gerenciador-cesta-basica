4a3865bf1247c5ce0e7209a6b90c69e9fe51c026
IVAN AMADO CARDOSO <peacevan@hotmail.com>
2026-03-31 16:53:50 -0300

    test: use createRoot in NotaFiscalUpload.int; enable React 18 act via jest.setup

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,5 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest'
   },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
---
+++ jest.setup.js
@@ -0,0 +1,3 @@
+// Enable React 18 act() support in the test environment
+// See: https://reactjs.org/docs/test-utils.html#act
+global.IS_REACT_ACT_ENVIRONMENT = true;
